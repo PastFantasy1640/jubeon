ca8134717c1f1700cf6400e51e3c05f4914c5de0
wgprojects2013 <pastfantasy1640@gmail.com>
2016-07-29 10:51:46 +0900

    不要なファイルの削除、mediaフォルダの更新
    昔のPanelInput系を削除
    mediaファイルを更新

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@
 #include "game/Layers/BackgroundLayer/BackgroundLayer.hpp"
 #include "game/Layers/FrameLayer/FrameLayer.hpp"
 #include "game/Layers/MusicInfoLayer/MusicInfoLayer.hpp"
-//#include "game/Layers/SequencePlayer/SequencePlayer.hpp"
-//#include "game/Layers/ShutterLayer/ShutterLayer.hpp"
+#include "game/Layers/SequencePlayer/SequencePlayer.hpp"
+#include "game/Layers/ShutterLayer/ShutterLayer.hpp"
 
 //for test
-//#include "game/Sequence.hpp"
-//#include "game/Music.hpp"
-//#include "game/PlayRecord.hpp"
+#include "game/Sequence.hpp"
+#include "game/Music.hpp"
+#include "game/PlayRecord.hpp"
 
 
 #include "systems/Logger/Logger.hpp"
@@ -40,24 +40,24 @@ int main(int argc, char * argv[]) {
 	jubeat_online::game::layers::BackgroundLayer bg;
 	jubeat_online::game::layers::FrameLayer frame;
 	jubeat_online::game::layers::MusicInfoLayer musicinfo;
-//	jubeat_online::game::layers::ShutterLayer shutterlayer;
+	jubeat_online::game::layers::ShutterLayer shutterlayer;
 	
-//	jubeat_online::game::Sequence seq("hogehoge");
-//	jubeat_online::game::Music mus;
+	jubeat_online::game::Sequence seq("hogehoge");
+	jubeat_online::game::Music mus;
 
 	//まずmusicに何か指定
 	//ここはアクセサを介した実装へ
-//	mus.soundbuffer.loadFromFile("media/demo.flac");
-//	mus.soundplayer.setBuffer(mus.soundbuffer);
+	mus.soundbuffer.loadFromFile("media/demo.flac");
+	mus.soundplayer.setBuffer(mus.soundbuffer);
 
 	//このデータをいじって、あらかじめjudgedに入れておけば自動プレイ（リプレイ）が可能
-//	std::unique_ptr<jubeat_online::game::PlayRecord> playrecord(new jubeat_online::game::PlayRecord);
+	std::unique_ptr<jubeat_online::game::PlayRecord> playrecord(new jubeat_online::game::PlayRecord);
 	
-//	std::unique_ptr<std::list<jubeat_online::game::PlayRecord::PanelInput>> pi_list(new std::list<jubeat_online::game::PlayRecord::PanelInput>());
-//	jubeat_online::game::PlayRecord::PanelInput tmp;
-//	tmp.j = jubeat_online::game::NOJUDGE;
+	std::unique_ptr<std::list<jubeat_online::game::PlayRecord::PanelInput>> pi_list(new std::list<jubeat_online::game::PlayRecord::PanelInput>());
+	jubeat_online::game::PlayRecord::PanelInput tmp;
+	tmp.j = jubeat_online::game::NOJUDGE;
 
-/*	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 10; i++) {
 		tmp.ms = 4000 + i * 4000; tmp.panel_no = 0; tmp.t = jubeat_online::game::PlayRecord::PUSH;
 		pi_list->push_back(tmp);
 		tmp.ms = 5000 + i * 4000; tmp.panel_no = 4; tmp.t = jubeat_online::game::PlayRecord::PUSH;
@@ -87,12 +87,12 @@ int main(int argc, char * argv[]) {
 
 	jubeat_online::game::layers::SequencePlayer seqplayer(&seq, &mus, std::move(playrecord));
 
-*/
+
 	a.addLayer(&bg, jubeat_online::graphics::layer::LayerManager::BACKGROUND, 0);
 	a.addLayer(&frame, jubeat_online::graphics::layer::LayerManager::FOREGROUND, 0);
 	a.addLayer(&musicinfo, jubeat_online::graphics::layer::LayerManager::MAIN, 0);
-//	a.addLayer(&shutterlayer, jubeat_online::graphics::layer::LayerManager::MAIN, 0);
-//	a.addLayer(&seqplayer, jubeat_online::graphics::layer::LayerManager::MAIN, 0);	//上に追加。番号は重複しても全然問題ない。
+	a.addLayer(&shutterlayer, jubeat_online::graphics::layer::LayerManager::MAIN, 0);
+	a.addLayer(&seqplayer, jubeat_online::graphics::layer::LayerManager::MAIN, 0);	//上に追加。番号は重複しても全然問題ない。
 	
 	
 
