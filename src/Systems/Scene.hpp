5c020965d0a3352b02c51129e7c173bd3c29b5d4
pastfantasy1640 <pastfantasy1640@gmail.com>
2016-08-03 22:13:04 +0900

    Sceneクラスのヘッダを実装
    Sceneクラスが仮想関数を含んでてもstaticメソッドは呼べるか要調査。またstaticメソッドはオーバーライドできない

--- src/Systems/Scene.hpp
+++ src/Systems/Scene.hpp
@@ -1,1 +1,30 @@
 #pragma once
+
+#ifndef JUBEON_SYSTEMS_SCENE_HPP
+#define JUBEON_SYSTEMS_SCENE_HPP
+
+#include <memory>
+
+namespace jubeon {
+	namespace system {
+
+		//Sceneクラス
+		class Scene {
+		private:
+			//次のシーン
+			std::unique_ptr<Scene> next_scene;
+
+		protected:
+			virtual void setNextScene(std::unique_ptr<Scene> next_scene) final;
+
+		public:
+			virtual int process(void) = 0;
+
+			static int process(std::unique_ptr<Scene> first_scene);
+
+		};	
+	};
+};
+
+#endif
+
