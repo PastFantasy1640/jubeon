1968b49f9bd98d6517cc72ed7c277f65e9afe3a5
pastfantasy1640 <pastfantasy1640@gmail.com>
2016-08-03 22:22:39 +0900

    Scene.hppにコメント追加

--- src/Systems/Scene.hpp
+++ src/Systems/Scene.hpp
@@ -14,12 +14,29 @@ namespace jubeon {
 			//次のシーン
 			std::unique_ptr<Scene> next_scene;
 
+			//すでにstatic processメソッドは動いて居るか
+			//スレッドアンセーフ
+			bool is_running;
+
 		protected:
+			//次のシーンを設定する
 			virtual void setNextScene(std::unique_ptr<Scene> next_scene) final;
 
 		public:
+			//コンストラクタ
+			Scene() {};
+
+			//デストラクタ
+			virtual ~Scene() {};
+
+			//process仮想関数。それぞれのシーンで処理を記述する
+			//プロセスから抜けるとすぐにNextSceneへ行くので、ループは自前で整備する。
+			//戻り値が0以外だとプログラムを終了する。
 			virtual int process(void) = 0;
 
+			//メインスレッドで実行されるprocess関数。
+			//再帰してはならないし、できない設計にしてある
+			//メインスレッドで実行する。戻り値は終了ステータス
 			static int process(std::unique_ptr<Scene> first_scene);
 
 		};	
