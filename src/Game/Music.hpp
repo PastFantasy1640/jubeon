c5ee4f1dc1c09494ecfad8f063b3b15144424006
pastfantasy1640 <pastfantasy1640@gmail.com>
2016-08-06 01:11:02 +0900

    Music.hppを整備#1

--- src/Game/Music.hpp
+++ src/Game/Music.hpp
@@ -3,14 +3,22 @@
 #ifndef jubeon_MUSIC_HPP
 #define jubeon_MUSIC_HPP
 
-#include <mutex>
+#include <array>
+#include <memory>
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 namespace jubeon {
 	namespace game {
 		class Music {
 		public:
 			
+			//コンストラクタ
+			Music(
+
+
+
+
 			//再生中の時間を取得する関数（スレッドセーフ）
 			unsigned int getPlayingCurrentTime(void) const;
 
@@ -27,13 +35,29 @@ namespace jubeon {
 			sf::Sound soundplayer;
 		private:
 
-			//スレッドセーフ対策
-		//	std::mutex mtx;
+			//メンバー変数
+			int									music_id;					//楽曲ID
+			const std::string					metafile_path;				//メタファイルパス
+			std::array<std::string, 3>			notes_filepath;				//ノーツの譜面ファイルパス
+			std::array<int, 3>					high_score;					//ハイスコア
+			std::array<std::string, 3>	high_score_pr_filepath;		//プレイ記録ファイル（最高）のファイルパス
+				
+			std::array<std::string, 3>	latest_score_pr_filepath;	//プレイ記録ファイル（直近）のファイルパス
 
+			const std::string					name;						//曲名
+			const std::string					artist;						//アーティスト名
+			const std::string					sound_filepath;				//楽曲ファイルパス
 
+			std::unique_ptr<sf::SoundBuffer>	up_sound_buffer;			//バッファ
+			const sf::Sound						sound;						//プレイヤー
+			const sf::Image						thumbnail;					//サムネ
+			
+			//TO DO : MusicBarは未定義
+			//std::array<MusicBar, 3>	musicbar_challenge;	//ミュージックバーチャレンジ
+			
 
-
-
+			//例のごとくデフォルトコンストラクタは禁止。
+			Music(void);
 		};
 	}
 }
