9de64e1c51facb868a6d4d8f12edc2551e68e943
wgprojects2013 <pastfantasy1640@gmail.com>
2016-08-09 11:15:25 +0900

    型宣言などを分離させた

---
+++ src/Game/NoteDefinition.hpp
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////
+// (c) 2016 white NoteDefition.hpp
+////////////////////////////////////////////////
+
+#pragma once
+
+#ifndef JUBEON_GAME_NOTEDEFINITION_HPP
+#define JUBEON_GAME_NOTEDEFINITION_HPP
+
+//namespace is jubeon
+namespace jubeon {
+	typedef int jMillisec;			//É~Éäïb
+	typedef unsigned char jPanel;	//ÉpÉlÉãî‘çÜ
+}
+
+#endif
---
+++ src/Game/TypeDefinition.hpp
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////
+// (c) 2016 white TypeDefinition.hpp
+///////////////////////////////////////////////////////
+
+#pragma once
+
+#ifndef jubeon_GAME_JUDGEDEFINITION_HPP
+#define jubeon_GAME_JUDGEDEFINITION_HPP
+
+//namespace is jubeon
+namespace jubeon {
+	enum Type : bool {
+		PUSH = true,
+		RELEASE = false
+	};
+}
+
+#endif
