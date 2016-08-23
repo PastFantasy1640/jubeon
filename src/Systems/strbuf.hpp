2c6652dd9790daac62124d060848440eb735ce3b
PastFantasy1640 <pastfantasy1640@gmail.com>
2016-08-23 09:49:36 +0900

    統括するインクルードファイル

---
+++ src/Systems/strbuf.hpp
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////
+// (c) 2016  white   strbuf library
+////////////////////////////////////////////////////////////
+
+#pragma once
+
+#ifndef STRBUF_STRBUF_HPP
+#define STRBUF_STRBUF_HPP
+
+#include "StreamBuffer.hpp"
+#include "InputStreamBuffer.hpp"
+#include "OutputStreamBuffer.hpp"
+
+#endif
