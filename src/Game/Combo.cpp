a7e4983d019a9db775b7b8e0227cda388cf35b38
burugaria7 <burugaria@icloud.com>
2017-01-05 00:18:18 +0900

    Comboクラスを追加

---
+++ src/Game/Combo.cpp
@@ -0,0 +1,31 @@
+#include "Combo.hpp"
+
+unsigned int jubeon::game::Combo::increase(void)
+{
+	this->combo ++;
+
+	return this->combo;
+}
+
+void jubeon::game::Combo::reset(void)
+{
+	this->combo = 0;
+}
+
+unsigned int jubeon::game::Combo::get(void) const
+{
+	return this->combo;
+}
+
+bool jubeon::game::Combo::isUpdate(void) const
+{
+	if (this->combo != this->combo_baf) {
+		return true;
+	}
+	return false;
+}
+
+void jubeon::game::Combo::clearUpdateCount(void)
+{
+	this->combo_baf = this->combo;
+}
---
+++ src/Game/Combo.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#ifndef JUBEON_GAME_COMBO_HPP
+#define JUBEON_GAME_COMBO_HPP
+
+namespace jubeon {
+	namespace game {
+		class Combo {
+
+		private:
+
+			unsigned int combo;
+
+			unsigned int combo_baf;
+
+		public:
+
+			//コンボを1増やす。 戻り値:増やした後のコンボ数
+			unsigned int increase(void);
+
+			//コンボを絶やす（０にする）
+			void reset(void);
+
+			//現在のコンボ数を返す。 戻り値:現在のコンボ数
+			unsigned int get(void) const;
+
+			//下記メソッドclearUpdateCountが呼ばれてから値に変更があったか検知する。 戻り値:[true]変更あり。
+			bool isUpdate(void) const;
+
+			//値変更の検知フラグを伏せる。
+			void clearUpdateCount(void);
+
+
+		
+		};
+
+	}
+}
+
+
+#endif
