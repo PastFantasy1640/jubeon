ce1018111d35c5117a5f2aa81c08d268009d220b
burugaria7 <burugaria@icloud.com>
2017-01-30 20:22:29 +0900

    ComboLayerクラスの追加

---
+++ src/Game/Layers/ComboLayer.cpp
@@ -0,0 +1,13 @@
+#include "ComboLayer.hpp"
+
+void jubeon::game::layers::ComboLayer::Init()
+{
+}
+
+void jubeon::game::layers::ComboLayer::Draw()
+{
+}
+
+void jubeon::game::layers::ComboLayer::Exit()
+{
+}
---
+++ src/Game/Layers/ComboLayer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef jubeon_COMBO_LAYER_HPP
+#define jubeon_COMBO_LAYER_HPP
+
+#include "../../Graphics/Layer/LayerBase.hpp"
+#include <SFML/Graphics.hpp>
+#include "../../Game/Combo.hpp"
+
+namespace jubeon {
+	namespace game {
+		namespace layers {
+			class ComboLayer : public jubeon::graphics::LayerBase {
+			private:
+				sf::Texture dummy;
+
+
+			public:
+				virtual void Init() override;
+				virtual void Draw() override;
+				virtual void Exit() override;
+		
+			};
+		}
+	}
+}
+
+#endif
