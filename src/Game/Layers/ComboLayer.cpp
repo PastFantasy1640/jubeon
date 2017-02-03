1a7145f187f2e738d4301817e4841248f35c7af6
burugaria7 <burugaria@icloud.com>
2017-02-03 19:48:59 +0900

    座標の計算代入とリスタート処理の追加

--- src/Game/Layers/ComboLayer.cpp
+++ src/Game/Layers/ComboLayer.cpp
@@ -1,4 +1,5 @@
 #include "ComboLayer.hpp"
+#include <time.h>
 
 void jubeon::game::layers::ComboLayer::Init()
 {
@@ -11,3 +12,37 @@ void jubeon::game::layers::ComboLayer::Draw()
 void jubeon::game::layers::ComboLayer::Exit()
 {
 }
+
+void jubeon::game::layers::ComboLayer::Restart()
+{
+	if (combo.isUpdate == true) {
+		
+		this->t0 = clock();
+
+		//y座標をデフォルトに戻す
+		this->y = this->y_def;
+	}
+}
+
+void jubeon::game::layers::ComboLayer::Set_Coordinates()
+{
+	this->t1 = clock();
+
+	this->t_ = this->t1 - this->t0;
+
+	//上がりきるまでの座標を計算代入
+	if (this->t_ <= 40) {
+		this->y = this->y_def+1 / 8 * this->t_;
+	}
+
+	//下がりきるまでの座標を計算代入
+	if (40 < this->t_ <= 160) {
+		this->y = this->y_def - 1 / 24 * this->t_;
+	}
+
+	//下がりきったあとの座標を計算代入
+	if (160 < this->t_) {
+		this->y = this->y_def;
+	}
+}
+
--- src/Game/Layers/ComboLayer.hpp
+++ src/Game/Layers/ComboLayer.hpp
@@ -14,12 +14,35 @@ namespace jubeon {
 			private:
 				sf::Texture dummy;
 
+				Combo combo;
+
+				//sf;;clock的なのに修正しなきゃ
+				clock_t t0;//最初に記録
+				clock_t t1;//次に記録
+				clock_t t_;//時間の差
+
+				//描写の時に使いそうな...
+				int combo_baf;
+				char combo_baff;
+
+				//描写用の座標
+				int y;
+				int x1, x2, x3, x4;
+
+				//初期のy座標
+				int y_def;
 
 			public:
 				virtual void Init() override;
 				virtual void Draw() override;
 				virtual void Exit() override;
-		
+
+				//値変更の検知フラグを見て、リスタート処理
+				void Restart();
+
+				//時間に応じて座標を上下する処理
+				void Set_Coordinates();
+			
 			};
 		}
 	}
