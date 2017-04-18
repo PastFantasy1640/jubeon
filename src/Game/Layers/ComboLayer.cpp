78d7c8034547cd130f2ef12141939b008e7d6cd4
burugaria7 <burugaria@icloud.com>
2017-04-19 00:09:39 +0900

    処理の追加等

--- src/Game/Layers/ComboLayer.cpp
+++ src/Game/Layers/ComboLayer.cpp
@@ -1,11 +1,84 @@
 #include "ComboLayer.hpp"
 
+
+#include "Storages/Resource.hpp"
+
 void jubeon::game::layers::ComboLayer::Init()
 {
+
+	storages::Resource::setf("media/image/combo_0.png", storages::Resource::TEX).setKey("ComboLayer.combo0");
+	storages::Resource::setf("media/image/combo_1.png", storages::Resource::TEX).setKey("ComboLayer.combo1");
+	storages::Resource::setf("media/image/combo_2.png", storages::Resource::TEX).setKey("ComboLayer.combo2");
+	storages::Resource::setf("media/image/combo_3.png", storages::Resource::TEX).setKey("ComboLayer.combo3");
+	storages::Resource::setf("media/image/combo_4.png", storages::Resource::TEX).setKey("ComboLayer.combo4");
+	storages::Resource::setf("media/image/combo_5.png", storages::Resource::TEX).setKey("ComboLayer.combo5");
+	storages::Resource::setf("media/image/combo_6.png", storages::Resource::TEX).setKey("ComboLayer.combo6");
+	storages::Resource::setf("media/image/combo_7.png", storages::Resource::TEX).setKey("ComboLayer.combo7");
+	storages::Resource::setf("media/image/combo_8.png", storages::Resource::TEX).setKey("ComboLayer.combo8");
+	storages::Resource::setf("media/image/combo_9.png", storages::Resource::TEX).setKey("ComboLayer.combo9");
+
+
+	for (int i = 0; i <= 10; i++) {
+		this->combo_sp[0].setTexture(*storages::Resource::setk("ComboLayer.combo0").gett());
+		this->combo_sp[1].setTexture(*storages::Resource::setk("ComboLayer.combo1").gett());
+		this->combo_sp[2].setTexture(*storages::Resource::setk("ComboLayer.combo2").gett());
+		this->combo_sp[3].setTexture(*storages::Resource::setk("ComboLayer.combo3").gett());
+		this->combo_sp[4].setTexture(*storages::Resource::setk("ComboLayer.combo4").gett());
+		this->combo_sp[5].setTexture(*storages::Resource::setk("ComboLayer.combo5").gett());
+		this->combo_sp[6].setTexture(*storages::Resource::setk("ComboLayer.combo6").gett());
+		this->combo_sp[7].setTexture(*storages::Resource::setk("ComboLayer.combo7").gett());
+		this->combo_sp[8].setTexture(*storages::Resource::setk("ComboLayer.combo8").gett());
+		this->combo_sp[9].setTexture(*storages::Resource::setk("ComboLayer.combo9").gett());
+	}
+
+
+	sprintf(combo_buf,"%d",combo->get());
+	keta1 = combo_buf[0];
+	keta2 = combo_buf[1];
+	keta3 = combo_buf[2];
+	keta4 = combo_buf[3];
+	
+	if (combo->get() >= 6 && combo->get() <= 9) {
+		this->combo_sp[keta1].setPosition(this->w / 3 * 8, get_Coordinates());
+	}
+	if (combo->get() >= 10 && combo->get() <= 99) {
+		this->combo_sp[keta1].setPosition(this->w / 4, get_Coordinates());
+		this->combo_sp[keta2].setPosition(this->w / 2, get_Coordinates());
+	}
+	if (combo->get() >= 100 && combo->get() <= 999) {
+		this->combo_sp[keta1].setPosition(this->w / 8, get_Coordinates());
+		this->combo_sp[keta2].setPosition(this->w / 8 * 3, get_Coordinates());
+		this->combo_sp[keta3].setPosition(this->w / 8 * 5, get_Coordinates());
+	}
+	if (combo->get() >= 1000 && combo->get() <= 9999) {
+		this->combo_sp[keta1].setPosition(0, get_Coordinates());
+		this->combo_sp[keta2].setPosition(this->w / 4, get_Coordinates());
+		this->combo_sp[keta3].setPosition(this->w / 2, get_Coordinates());
+		this->combo_sp[keta4].setPosition(this->w / 4 *3, get_Coordinates());
+	}
+
 }
 
 void jubeon::game::layers::ComboLayer::Draw()
 {
+	if (combo->get() >= 6 && combo->get() <= 9) {
+		this->draw(this->combo_sp[keta1]);
+	}
+	if (combo->get() >= 10 && combo->get() <= 99) {
+		this->draw(this->combo_sp[keta1]);
+		this->draw(this->combo_sp[keta2]);
+	}
+	if (combo->get() >= 100 && combo->get() <= 999) {
+		this->draw(this->combo_sp[keta1]);
+		this->draw(this->combo_sp[keta2]);
+		this->draw(this->combo_sp[keta3]);
+	}
+	if (combo->get() >= 1000 && combo->get() <= 9999) {
+		this->draw(this->combo_sp[keta1]);
+		this->draw(this->combo_sp[keta2]);
+		this->draw(this->combo_sp[keta3]);
+		this->draw(this->combo_sp[keta4]);
+	}
 }
 
 void jubeon::game::layers::ComboLayer::Exit()
@@ -20,35 +93,40 @@ void jubeon::game::layers::ComboLayer::restart()
 		clock.restart();
 
 		//y座標をデフォルトに戻す
-		this->y = this->y_def;
+		//this->y = this->y_def;
 
 	}
 }
 
-void jubeon::game::layers::ComboLayer::set_Coordinates()
+int jubeon::game::layers::ComboLayer::get_Coordinates()
 {
 	this->t = this->clock.getElapsedTime().asMilliseconds();
 
-	//this->t_ = this->t1 - this->t0;
 
 	//上がりきるまでの座標を計算代入
 	if (this->t <= 40) {
-		this->y = this->y_def+1 / 8 * this->t;
+		return this->y_def+1 / 8 * this->t;
 	}
 
 	//下がりきるまでの座標を計算代入
 	if (40 < this->t && this->t <= 160) {
-		this->y = this->y_def - 1 / 24 * this->t;
+		return this->y_def - 1 / 24 * this->t;
 	}
 
 	//下がりきったあとの座標を計算代入
 	if (160 < this->t) {
-		this->y = this->y_def;
+		return this->y_def;
 	}
 }
 
-jubeon::game::layers::ComboLayer::ComboLayer()
+jubeon::game::layers::ComboLayer::ComboLayer(int m)
 {
+	std::array<sf::Texture *, 10> textures;
+
+
+
+	//描写範囲を受け取るようにしなきゃ? これでいいのかな↓
+	this -> w = m;
 }
 
 jubeon::game::layers::ComboLayer::~ComboLayer()
--- src/Game/Layers/ComboLayer.hpp
+++ src/Game/Layers/ComboLayer.hpp
@@ -7,6 +7,7 @@
 #include <SFML/Graphics.hpp>
 #include "../../Game/Combo.hpp"
 #include <SFML/System/Clock.hpp>
+#include <array>
 
 namespace jubeon {
 	namespace game {
@@ -19,16 +20,37 @@ namespace jubeon {
 				ComboLayer::ComboLayer(Combo * combo) : combo(combo) {}
 
 
+				sf::Sprite combo_sp[10];
+
+				/*sf::Sprite combo0_sp;
+				sf::Sprite combo1_sp;
+				sf::Sprite combo2_sp;
+				sf::Sprite combo3_sp;
+				sf::Sprite combo4_sp;
+				sf::Sprite combo5_sp;
+				sf::Sprite combo6_sp;
+				sf::Sprite combo7_sp;
+				sf::Sprite combo8_sp;
+				sf::Sprite combo9_sp;*/
+
 				sf::Clock clock;
 				int t;
 
+
 				//描写の時に使いそうな...
-				int combo_buf;
-				char combo_buff;
+
+				int w; //描写範囲
+
+				//int combo_buf;
+				char combo_buf[4];
+				int keta1;
+				int keta2;
+				int keta3;
+				int keta4;
+
 
 				//描写用の座標
 				int y;
-				int x1, x2, x3, x4;
 
 				//初期のy座標
 				int y_def;
@@ -42,10 +64,10 @@ namespace jubeon {
 				void restart();
 
 				//時間に応じて座標を上下する処理
-				void set_Coordinates();
+				int get_Coordinates();
 
 
-				ComboLayer();
+				ComboLayer(int m);
 				~ComboLayer();
 			
 			};
